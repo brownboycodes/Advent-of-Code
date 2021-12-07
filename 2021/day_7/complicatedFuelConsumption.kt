8433217c457d21966b5d23c296c2915a17daed0c
brownboycodes <ideatorrraj@gmail.com>
2021-12-07 21:31:32 +0530

    improved code for part 2 of day 7 :zap::fire:

--- 2021/day_7/complicatedFuelConsumption.kt
+++ 2021/day_7/complicatedFuelConsumption.kt
@@ -7,21 +7,22 @@ fun findComplicatedPosition(positions: IntArray) {
     var fuelCostPerPosition = mutableMapOf<Int, Int>()
 
     var currentRunningCost: Int
-    var s: Int
+//    var s: Int
     var diff: Int
-    var currentFuel: Int
+//    var currentFuel: Int
     for (p1 in 0..positions.maxOrNull()!!) {
         if (!fuelCostPerPosition.containsKey(p1)) {
             currentRunningCost = 0
             for (p2 in positions) {
-                currentFuel = 0
-                s = 1
+//                currentFuel = 0
+//                s = 1
                 diff = abs(p1 - p2)
-                for (i in 1..diff) {
-                    currentFuel += s
-                    s++
-                }
-                currentRunningCost += currentFuel
+//                for (i in 1..diff) {
+//                    currentFuel += s
+//                    s++
+//                }
+//                currentRunningCost += currentFuel
+                currentRunningCost+=(1..diff).sum()
             }
 
             fuelCostPerPosition[p1] = currentRunningCost
